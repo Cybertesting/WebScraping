52325ea947e34b6964fd60f0b717a67723c56a5d
MadHatter <35674630+Cybertesting@users.noreply.github.com>
2019-06-13 17:30:56 -0400

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -1,11 +1,17 @@
-import request
+#training from freeCodeCamp.org created by Tutorial from Vincent Russo of Lucid Programming. Check out his YouTube channel: 
+
+
+
+import requests
 from bs4 import BeautifulSoup
 
-results=requests.get("")
-src=result.content
+result = requests.get("https://www.whitehouse.gov/briefings-statements/")
+src = result.content
 soup = BeautifulSoup(src, 'lxml')
 
-urls=[]
-for h2_tag in soup.find_all("h2"):
-  a_tag = h2_tag.find('a')
-  urls.append  
+urls = []
+for h2_tag in soup.find_all(h2):
+    a_tag = h2_tag.find('a')
+    urls.append(a_tag.attrs['href'])
+    
+print(urls)
